f4b73b649a376f60f1a5bbdd6d2637e0ed9ad406
martinrl142 <81473853+martinrl142@users.noreply.github.com>
2021-11-30 20:18:52 -0300

    Eliminar

--- notes-app-mono-repo-main/api/test-routes.js
+++
@@ -1,7 +0,0 @@
-const { app } = require('./index')
-
-console.log(
-  app._router.stack
-    .filter(route => route.route)
-    .map(route => route.route.path)
-)
--- notes-app-mono-repo-main/app/src/i18n/index.js
+++
@@ -1,5 +0,0 @@
-export default {
-  TOGGABLE: {
-    CANCEL_BUTTON: 'Exit'
-  }
-}
