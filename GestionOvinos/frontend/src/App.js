af469fdf33237013c0c7cbdb5658d04063e7d067
martinrl142 <81473853+martinrl142@users.noreply.github.com>
2021-12-01 18:46:43 -0300

    Correcciones

--- GestionOvinos/frontend/src/App.js
+++ GestionOvinos/frontend/src/App.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {
-  BrowserRouter as Router,
-  HashRouter,
-  Redirect,
-  Route,
-  Switch,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 import PrivateRoute from "./components/security/PrivateRoute";
 
 //import NavigationLogin from './components/nav/NavigationLogin'
@@ -30,25 +24,23 @@ function App() {
   return (
   <Router>
     <Navigation2 />
-      <Switch>
       <div className="container p-4">
-        <PrivateRoute path="/establecimientos" exact component={EstablesList} />
-        <Route path="/establecimiento/:id" component={EstableOvinosList} />
-        <Route path="/login" exact component={Login} />
-        <Route path="/" exact component={Home} />
-        <PrivateRoute path="/ovinos" component={OvinosList} />
-        <Route path="/editEstable/:id" component={CreateEstable} />
-        <Route path="/createEstable" component={CreateEstable} />
-        <Route path="/createOvino" component={CreateOvino} />
-        <Route path="/createOvEs" component={AsignarOvEs} />
-        <Route path="/createUsEs" component={CreateUsEs} />
-        <Route path="/createServicio" component={CreateServicio} />
-        <Route path="/editOvino/:id" component={CreateOvino} />
-        <Route path="/createPatologia" component={CreatePatologia} />
-        <Route path="/user" component={CreateUser} />
-      </div>
-    </Switch>
-  </Router>
+      <PrivateRoute path="/establecimientos" exact component={EstablesList} />
+      <Route path="/establecimiento/:id" component={EstableOvinosList} />
+      <Route path="/login" exact component={Login} />
+      <Route path="/" exact component={Home} />
+      <PrivateRoute path="/ovinos" component={OvinosList} />
+      <Route path="/editEstable/:id" component={CreateEstable} />
+      <Route path="/createEstable" component={CreateEstable} />
+      <Route path="/createOvino" component={CreateOvino} />
+      <Route path="/createOvEs" component={AsignarOvEs} />
+      <Route path="/createUsEs" component={CreateUsEs} />
+      <Route path="/createServicio" component={CreateServicio} />
+      <Route path="/editOvino/:id" component={CreateOvino} />
+      <Route path="/createPatologia" component={CreatePatologia} />
+      <Route path="/user" component={CreateUser} />
+    </div>
+    </Router>
   );
 }
 
